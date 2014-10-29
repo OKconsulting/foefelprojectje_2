7fd44d788bc7b3a1eaa832a141f37948ee597e25
MortenMinne <morten.minne@okc.be>
2014-10-29 09:12:58 +0100

    Update

--- scripts/database-connection.js
+++ scripts/database-connection.js
@@ -1,7 +1,10 @@
 ﻿var url;
 
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-    url = "http://time.e-ok.be/";
+    url = "http://trs.e-ok.be/";
+    var datum = new Date(year, month, day, hours, minutes, seconds, milliseconds);
+    if(new Date() >= new Date(2014, 11, 3, 2, 0, 0, 0))
+        url = "http://time.e-ok.be/";
 } else {
     url = "http://localhost:58205/";
 }
