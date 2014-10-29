a957fe65bc27c13afd7a452a1d3d953a1c250bc7
MortenMinne <morten.minne@okc.be>
2014-10-29 11:17:24 +0100

    Update
    De applicatie tot 10nov laten draaien op de test server daarna
    overschakelen naar de productie server

--- scripts/database-connection.js
+++ scripts/database-connection.js
@@ -2,8 +2,7 @@
 
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
     url = "http://trs.e-ok.be/";
-    var datum = new Date(year, month, day, hours, minutes, seconds, milliseconds);
-    if(new Date() >= new Date(2014, 11, 3, 2, 0, 0, 0))
+    if(new Date() >= new Date(2014, 10, 3, 2, 0, 0, 0))
         url = "http://time.e-ok.be/";
 } else {
     url = "http://localhost:58205/";
