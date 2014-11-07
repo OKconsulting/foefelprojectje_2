45c99eb0a045d37501b2d8b745bea0bb410efbbe
MortenMinne <morten.minne@okc.be>
2014-11-07 13:49:08 +0100

    Update

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -1,9 +1,7 @@
-﻿cordova.define('cordova/plugin_list', function (require, exports, module) {
-    module.exports = [];
-    module.exports.metadata =
-    // TOP OF METADATA
-    {
-        "org.apache.cordova.geolocation": "0.3.7"
-    }
-    // BOTTOM OF METADATA
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
 });
