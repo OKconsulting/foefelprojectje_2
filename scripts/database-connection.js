21416df68f4b5d87f6140518251c35f578a17601
MortenMinne <morten.minne@okc.be>
2015-01-06 14:50:14 +0100

    Revert "Update"
    This reverts commit 6f6d9798331fa413a8feee7098522ca7f654fe47.

---
+++ scripts/database-connection.js
@@ -0,0 +1,9 @@
+﻿var url;
+
+if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
+    url = "http://trs.e-ok.be/";
+    if(new Date() >= new Date(2014, 10, 3, 2, 0, 0, 0))
+        url = "http://time.e-ok.be/";
+} else {
+    url = "http://localhost:58205/";
+}
