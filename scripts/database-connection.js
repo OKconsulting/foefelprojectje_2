16510006d0563d20b71dbfd9f2cca46c1373c76b
MortenMinne <morten.minne@okc.be>
2015-03-12 11:17:27 +0100

    Update

--- scripts/database-connection.js
+++ scripts/database-connection.js
@@ -1,7 +1,8 @@
 ﻿var url;
 
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-    url = "http://192.168.2.11:58206/"; //"http://time.e-ok.be/";
+    url = "http://192.168.2.16:58206/";
+    //"http://time.e-ok.be/";
 } else {
     url = "http://localhost:58206/";
 }
