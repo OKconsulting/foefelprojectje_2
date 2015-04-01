a2833a989e2d35458a67132f9cc0a85e5e583a97
MortenMinne <morten.minne@okc.be>
2015-04-01 11:22:07 +0200

    Update

--- scripts/googleMap.js
+++ scripts/googleMap.js
@@ -13,8 +13,8 @@ function initialize() {
     }
 }
 
-var mapHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0); // $(document).height() / 100 * 75;
+var mapHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) / 100 * 75; // $(document).height() / 100 * 75;
 $('#myGoogleMap').css('height', mapHeight + 'px');
 
-var mapWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0); // $(document).width() / 100 * 75;
+var mapWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) / 100 * 75; // $(document).width() / 100 * 75;
 $('#myGoogleMap').css('width', mapWidth + 'px');
