3eb2b3e9347df0ad8a203c0b6a20f7d6010e019c
MortenMinne <morten.minne@okc.be>
2015-04-21 12:21:42 +0200

    Update

--- scripts/database-connection.js
+++ scripts/database-connection.js
@@ -1,8 +1,13 @@
-﻿var url;
+﻿// Bepalen welke url gebruikt moet worden
+var url;
 
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
     url = "http://trs.e-ok.be/";
-    //"http://time.e-ok.be/";
+
+    var vandaag = new Date();
+    if (vandaag >= new Date(2015, 4, 4, 0, 0, 0, 0)) {
+        url = "http://time.e-ok.be/";
+    }
 } else {
     url = "http://localhost:58206/";
 }
