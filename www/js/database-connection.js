442d0804438b54f1f35d28f02b54138349b9ab61
MortenMinne <morten.minne@okc.be>
2016-03-02 14:20:19 +0100

    Update

--- www/js/database-connection.js
+++ www/js/database-connection.js
@@ -2,9 +2,9 @@
 var url;
 
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-    url = "http://trs.e-ok.be/";
+    url = "http://192.168.2.125:58206/";
 } else {
-    url = "http://trs.e-ok.be/";
+    url = "http://192.168.2.125:58206/";
 }
 // http://time.e-ok.be/             // Live
 // http://localhost:58206/          // Lokaal
