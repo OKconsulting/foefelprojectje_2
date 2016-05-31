cf5f290e3213f22e5c6c8dd5856272894251acfc
MortenMinne <morten.minne@okc.be>
2016-05-31 14:53:08 +0200

    Naar https

--- platforms/wp8/www/js/database-connection.js
+++ platforms/wp8/www/js/database-connection.js
@@ -2,11 +2,11 @@
 var url;
 
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-    url = "http://192.168.2.125:58206/";
+    url = "https://time.e-ok.be/";
 } else {
-    url = "http://192.168.2.125:58206/";
+    url = "https://time.e-ok.be/";
 }
-// http://time.e-ok.be/             // Live
+// https://time.e-ok.be/             // Live
 // http://localhost:58206/          // Lokaal
 // http://trs.e-ok.be/              // Test
 // http://192.168.2.125:58206/      // Device
